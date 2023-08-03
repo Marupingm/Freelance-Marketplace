6884706361853a9758fd6a4c440c91bc71a47d7a
topsy <Malatjimj2@gmail.com>
2023-08-04 00:00:00 +0200

    Initial commit

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -107,3 +107,4 @@ productSchema.index({ rating: -1 });
 productSchema.index({ salesCount: -1 });
 
 export default mongoose.models.Product || mongoose.model<IProduct>('Product', productSchema); //  
+//  
